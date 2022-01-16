be64e4d8776956950d5d3bf0feace1d731dc5dc1
Furkan Kaplan <furkankaplan@Furkans-MacBook-Pro.local>
2022-01-16 19:09:51 +0300

    Add argument parser dependency

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Logify",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -15,7 +14,11 @@ let package = Package(
         .executableTarget(
             name: "Logify",
             dependencies: ["LogifyCore"]),
-        .target(name: "LogifyCore"),
+        .target(
+            name: "LogifyCore",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]),
         .testTarget(
             name: "LogifyTests",
             dependencies: ["Logify"]),
