17e4f4953274cb13a77d816242202bb1ee6aa3dd
Furkan Kaplan <furkankaplan@Furkans-MacBook-Pro.local>
2022-01-16 19:37:01 +0300

    Rename Logify file

--- Sources/LogifyCore/Logifier.swift
+++ Sources/LogifyCore/Logifier.swift
@@ -1,5 +1,5 @@
 //
-//  Logify.swift
+//  Logifier.swift
 //  xcode-colorful-log
 //
 //  Created by Furkan Kaplan on 13.01.2022.
@@ -8,12 +8,14 @@
 import Foundation
 import ArgumentParser
 
-struct Logify: ParsableCommand {
+public struct Logifier: ParsableCommand {
     
     @Argument(help: "Composition of subsystem and category with colon seperator. Ex: sub.system:category.name")
     var filter: String
     
-    mutating func run() throws {
+    public init() { }
+    
+    mutating public func run() throws {
         guard filter.split(separator: ":").count == 2 else {
             print("Invalid parameter. Valid parameter must be like that subsystem:category")
             return
